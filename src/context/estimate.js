b87c256448d1a7d33dfe384f796a315760a3d4df
runtime <erik.kroha@gmail.com>
2024-07-08 13:42:11 -0400

    next button now saves an estimate obj {"total":1} from the field. trying to add useEstimateService to avoid having to create the api now but seems to be difficult

--- src/context/estimate.js
+++ src/context/estimate.js
@@ -1,5 +1,6 @@
 import {createContext, useState} from 'react';
-import { useEstimatesService } from "../services/useEstimatesService";
+import calculateEstimate  from "../services/useEstimatesService";
+import axios from "axios";
 
 //CONTEXT PROVIDER
 // children are the
@@ -18,11 +19,36 @@ function Provider( {children} ) {
       //Fetch Data
     }
 
-    const createEstimate = (obj) => {
+    // const createEstimate = (obj) => {
+    //     /// MVP Working
+    //     console.log('[Provider] createEstimate: ', obj );
+    //     setEstimate(obj);
+    //     console.log(estimate.total);
+    //     //// json server
+    //
+    // }
+
+    // const createEstimate = async (obj) => {
+    //     //todo call estimateService and have algo give back the estimate
+    //     console.log('[Provider] createEstimate: ', obj );
+    //     const response = await axios.post('http://localhost:3001/estimates', {
+    //         obj
+    //     });
+    //     console.log('Provider] createEstimate response.data ', response.data);
+    //     const updatedEstimate = response.data;
+    //     setEstimate(updatedEstimate);
+    // }
+
+    const createEstimate = async (obj) => {
+        //call estimateService and have algo give back the estimate
         console.log('[Provider] createEstimate: ', obj );
-        setEstimate(obj);
-        console.log(estimate.total);
+        const response = await calculateEstimate(obj)
+        console.log('[Provider] createEstimate response: ', response);
+        //const updatedEstimate = response.data;
+        //setEstimate(updatedEstimate);
     }
+
+
 
     // const getEstimate = async () => {
     //     const response = await axios.get('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
