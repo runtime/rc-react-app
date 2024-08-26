47ca5e55f7254adcc1e879c8b64931bb29607920
runtime <erik.kroha@gmail.com>
2024-08-26 16:10:18 -0400

    connected estimate, user & location to calendar component and prefilled an apppointment for the amount of hours the estimate will take

--- src/pages/Appointments.js
+++ src/pages/Appointments.js
@@ -35,71 +35,71 @@ const Appointments = () => {
     // stub data
 
 
-    const cal = <Calendar />
+    //const cal = <Calendar />
 
     // IF we have an estimate with the right data structure but not one from the user
     // SHOW USER CREATE
-    // if ((estimate.hasOwnProperty("servicedetails") && (!user.hasOwnProperty("userdetails")))) {
-    //     content = <>
-    //         <Grid>
-    //             <Typography color="secondary" variant="cardTitle" component="h1" display="inline">Enter </Typography>
-    //             <Typography color="primary" variant="cardTitle" component='h1' display="inline">Information</Typography>
-    //             <Typography variant="body1" marginBottom='20px'>You are one step closer to enjoying a pristine space with our {estimate.servicedetails.typeofservice} service, featuring expert vacuuming of carpets and floors, precise dusting of every corner, and efficient mopping for a flawless finish..</Typography>
-    //
-    //             <Typography variant="h4" color="primary" marginTop='20px' marginBottom='20px'>In order for us to hold a {estimate.servicedetails.typeofservice} of
-    //                 your {estimate.servicedetails.numrooms} BR, {estimate.servicedetails.numbaths} BA {estimate.servicedetails.construct} for ${estimate.servicedetails.cost.total} will need some information.
-    //             </Typography>
-    //            {/* <Typography variant="body2" marginBottom='0px'>
-    //                 Your Anonymous user ID: <b>{estimate.servicedetails.userID}</b>
-    //             </Typography>*/}
-    //             {/*<Typography variant="body2" marginBottom='20px'>*/}
-    //             {/*    Your Personalized Estimate ID: <b>{estimate.id} </b>*/}
-    //             {/*</Typography>*/}
-    //         </Grid>
-    //         {/*<Typography variant="h3" marginBottom='20px'>Select a Date:</Typography>*/}
-    //
-    //         <UserCreate />
-    //     </>
-    //     // IF wehave an estimate with the correct data structure AND we have the user data
-    //     // SHOW USER DETAIL
-    //     // TODO get the views right when you have a user we should see the userdetail screen welcoming them and asking for their address
-    // } else if ((estimate.hasOwnProperty("servicedetails") && (user.hasOwnProperty("userdetails")))) {
-    //    content =
-    //        <UserDetail />
-    // } else {
-    //     //console.log('[EstimateDetail] (else if) estimate.servicedetails', estimate.servicedetails);
-    //     content =
-    //         <>
-    //             <Grid>
-    //                 <Typography color="secondary" variant="cardTitle" component="h1" display="inline">New </Typography>
-    //                 <Typography color="primary" variant="cardTitle" component='h1' display="inline">Service</Typography>
-    //                 <Typography variant="body1" marginBottom='20px'>
-    //                     Get a Free Estimate before Booking. No personal information needed. No harassing phone calls or emails.  Book anytime.
-    //                 </Typography>
-    //                 {/*<Button onClick={handleEstimateClick}>*/}
-    //                 {/*    Get Instant Free Estimate*/}
-    //                 {/*</Button>*/}
-    //                 <CardActions>
-    //                     <Button
-    //
-    //                         variant="contained"
-    //                         color="primary"
-    //                         type="Submit"
-    //                         onClick = {handleEstimateClick}
-    //                         className='classes button'>
-    //                         New Estimate
-    //                     </Button>
-    //                 </CardActions>
-    //             </Grid>
-    //
-    //             <Grid marginTop = '40px'>
-    //                 <RepeatService />
-    //             </Grid>
-    //
-    //
-    //         </>
-    //
-    // }
+    if ((estimate.hasOwnProperty("servicedetails") && (!user.hasOwnProperty("userdetails")))) {
+        content = <>
+            <Grid>
+                <Typography color="secondary" variant="cardTitle" component="h1" display="inline">Enter </Typography>
+                <Typography color="primary" variant="cardTitle" component='h1' display="inline">Information</Typography>
+                <Typography variant="body1" marginBottom='20px'>You are one step closer to enjoying a pristine space with our {estimate.servicedetails.typeofservice} service, featuring expert vacuuming of carpets and floors, precise dusting of every corner, and efficient mopping for a flawless finish..</Typography>
+
+                <Typography variant="h4" color="primary" marginTop='20px' marginBottom='20px'>In order for us to hold a {estimate.servicedetails.typeofservice} of
+                    your {estimate.servicedetails.numrooms} BR, {estimate.servicedetails.numbaths} BA {estimate.servicedetails.construct} for ${estimate.servicedetails.cost.total} will need some information.
+                </Typography>
+               {/* <Typography variant="body2" marginBottom='0px'>
+                    Your Anonymous user ID: <b>{estimate.servicedetails.userID}</b>
+                </Typography>*/}
+                {/*<Typography variant="body2" marginBottom='20px'>*/}
+                {/*    Your Personalized Estimate ID: <b>{estimate.id} </b>*/}
+                {/*</Typography>*/}
+            </Grid>
+            {/*<Typography variant="h3" marginBottom='20px'>Select a Date:</Typography>*/}
+
+            <UserCreate />
+        </>
+        // IF wehave an estimate with the correct data structure AND we have the user data
+        // SHOW USER DETAIL
+        // TODO get the views right when you have a user we should see the userdetail screen welcoming them and asking for their address
+    } else if ((estimate.hasOwnProperty("servicedetails") && (user.hasOwnProperty("userdetails")))) {
+       content =
+           <UserDetail />
+    } else {
+        //console.log('[EstimateDetail] (else if) estimate.servicedetails', estimate.servicedetails);
+        content =
+            <>
+                <Grid>
+                    <Typography color="secondary" variant="cardTitle" component="h1" display="inline">New </Typography>
+                    <Typography color="primary" variant="cardTitle" component='h1' display="inline">Service</Typography>
+                    <Typography variant="body1" marginBottom='20px'>
+                        Get a Free Estimate before Booking. No personal information needed. No harassing phone calls or emails.  Book anytime.
+                    </Typography>
+                    {/*<Button onClick={handleEstimateClick}>*/}
+                    {/*    Get Instant Free Estimate*/}
+                    {/*</Button>*/}
+                    <CardActions>
+                        <Button
+
+                            variant="contained"
+                            color="primary"
+                            type="Submit"
+                            onClick = {handleEstimateClick}
+                            className='classes button'>
+                            New Estimate
+                        </Button>
+                    </CardActions>
+                </Grid>
+
+                <Grid marginTop = '40px'>
+                    <RepeatService />
+                </Grid>
+
+
+            </>
+
+    }
     return (
         <div className='Estimates'>
             <Box>
@@ -109,8 +109,8 @@ const Appointments = () => {
                         <CardContent>
                             <Box sx={{ minWidth: 120 }}>
                                 <Typography color="black" display="inline">
-                                    {/*{content}*/}
-                                    {cal}
+                                    {content}
+                                    {/*{cal}*/}
                                 </Typography>
                             </Box>
                         </CardContent>
