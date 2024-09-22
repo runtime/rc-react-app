19b51806399fc514e2fcf7fe57d5cf1ac193c6ab
runtime <erik.kroha@gmail.com>
2024-09-21 21:51:51 -0400

    basic logic for contact.js to show content based on estimate state

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,28 @@
+import '../styles/Contact.css'
+import React from "react";
+import {
+    Card,
+    Grid,
+    Box,
+    Typography, ThemeProvider, CssBaseline, Button
+} from '@mui/material';
+import { RapidCleanTheme } from "../themes/Theme.js";
+import {useContext} from "react";
+import EstimateContext from "../context/estimate";
+
 const Contact = () => {
+    const {estimate} = useContext(EstimateContext);
+    console.log('[Contact] estimate ', estimate);
+    let content;
+    if (estimate.hasOwnProperty("servicedetails")) {
+        content = <div> {estimate.servicedetails.cost.total} </div>
+    } else {
+        content = <div> Loading... </div>
+    }
+
     return (
         <div>
-            <h1>Contact Us</h1>
+            {content}
         </div>
     )
 }
